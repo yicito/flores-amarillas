fc92c8a79e49a786196f9b110fab4f46c12fe724
yicito <jorgeyi99@gmail.com>
2026-08-01 13:34:48 -0300

    test

--- New folder/script.js
+++ New folder/script.js
@@ -2,9 +2,6 @@ const boton = document.getElementById("abrir");
 const mensaje = document.getElementById("mensaje");
 
 boton.addEventListener("click", () => {
-
     boton.style.display = "none";
-
     mensaje.style.display = "block";
-
 });
